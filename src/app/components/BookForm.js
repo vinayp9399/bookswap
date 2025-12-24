dad069da43202e5d26c780498b00578a9d4d63cb
Vinay Phatarpekar <vinayp9399@gmail.com>
2025-12-24 12:22:09 +0530

    done

--- src/app/components/BookForm.js
+++ src/app/components/BookForm.js
@@ -19,7 +19,14 @@ return (
 <div style={{textAlign:"center"}}><form style={{margin:"auto", width:"500px"}} onSubmit={handleSubmit}>
 <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Title" required />
 <input value={author} onChange={e => setAuthor(e.target.value)} placeholder="Author" required />
-<input value={condition} onChange={e => setCondition(e.target.value)} placeholder="Condition" required />
+<select value={condition} onChange={(e) => setCondition(e.target.value)}>
+  <option value="">Select condition</option>
+  <option value="new">New</option>
+  <option value="like_new">Like New</option>
+  <option value="very_good">Very Good</option>
+  <option value="good">Good</option>
+  <option value="acceptable">Acceptable</option>
+</select>
 <input value={image} onChange={e => setImage(e.target.value)} placeholder="Image URL" />
 <button type="submit">Post Book</button>
 </form></div>
